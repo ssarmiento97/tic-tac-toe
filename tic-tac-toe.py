d54dd1de436a782e1e236bebad643bc5de7a6719
Sam <samuel.sarmiento.1997@gmail.com>
2021-06-21 05:33:21 -0400

    added play_on function and start of main game loop

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,13 @@
-game_board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+game_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+keep_playing = True
+
+input("Welcome players. Press ENTER to start game.")
+
+while keep_playing:
+    display_board(game_board)
+
+
+    
 
 def display_board(current_board):
     print(f' {current_board[6]} | {current_board[7]} | {current_board[8]} ')
@@ -7,4 +16,19 @@ def display_board(current_board):
     print('------------')
     print(f' {current_board[0]} | {current_board[1]} | {current_board[2]} ')
 
-display_board(game_board)
+def play_on():
+    # ask the user if they would like to play again
+
+    user_choice = 'c'
+
+    # make sure user is selecting a valid option
+    while user_choice not in ['Y', 'N']:
+        user_choice = input("Would you like to play again? (Y/N): ").upper()
+
+        if user_choice not in ['Y', 'N']:
+            print("Invalid choice. Please input Y or N.")
+
+    if user_choice == 'Y':
+        return True
+    else:
+        return False
