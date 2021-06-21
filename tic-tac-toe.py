3073d287350170562f589eaf49aed13f035d9190
Sam <samuel.sarmiento.1997@gmail.com>
2021-06-21 06:02:14 -0400

    Added input functionality along w/ basic input verification

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,10 +1,19 @@
 game_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 keep_playing = True
+current_player = 1
 
+print("The board will be numbered as follows:")
+display_board(['1', '2', '3 ', '4', '5', '6', '7', '8', '9'])
 input("Welcome players. Press ENTER to start game.")
+print('\n'*5)
 
 while keep_playing:
+
+    print('Player 1 = X | Player 2 = O')
     display_board(game_board)
+    game_board = get_input(current_player, game_board)
+
+    break
 
 
     
@@ -32,3 +41,11 @@ def play_on():
         return True
     else:
         return False
+
+def get_input(player, current_board):
+    selection = "Invalid"
+    acceptable_range = range(1, 10)
+
+    if player % 2 == 1:
+        while not selection.isdigit() and selection not in acceptable_range:
+            selection = input("Player 1 please select where you would like to place your 'X' (1-9): ")
